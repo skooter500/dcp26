ba995b7edacfa747d54249dc329530b69e26e85e
skooter500 <skooter500@gmail.com>
2026-09-23 10:11:18 +0100

    x

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Welcome to the Metaverse
 
 ## Assessment
 - 20% Weekly submissions
-- [30% Assignment](https://github.com/skooter500/dcp25-assignment)
+- [30% Assignment/Test]()
 - 50% Exam
 
 # Resources
---
+++ week1/hello_python.py
@@ -0,0 +1,23 @@
+# declare a variable
+a = 10
+name = "Bryan"
+print(a)
+print(name)
+
+# get input
+name = input("What is your name?")
+print(name)
+
+# if statement
+if name == "Bryan" or name == "skooter500":
+    print(f"User {name} is a leprechaun!")
+else:
+    print(f"User {name} is a K-Pop star!")
+
+# loops
+for c in name:
+    print(c)
+
+for i in range(len(name)):
+    print(name[i])
+
